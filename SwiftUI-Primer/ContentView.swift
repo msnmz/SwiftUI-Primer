aeb822a89caa26c6b15712896ca8397af32788b5
msnmz <snmez.muharrem@gmail.com>
2020-01-29 18:57:05 +0100

    Initial commit

---
+++ SwiftUI-Primer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Primer
+//
+//  Created by User on 29/01/2020.
+//  Copyright © 2020 Muharrem Sonmez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
